4419d6cce78b3bc0a57941e498bdf673f74272a9
SAMKE-glitch <sammythemwa@gmail.com>
2024-02-10 16:45:44 +0300

    space

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -63,7 +63,8 @@ def reset_password_token(email: str) -> str:
 def update_password(email: str, reset_token: str, new_password: str) -> None:
     response = requests.put(
         f"{BASE_URL}/reset_password",
-        data={"email": email, "reset_token": reset_token, "new_password": new_password}
+        data={"email": email, "reset_token": reset_token,
+        "new_password": new_password}
     )
     assert response.status_code == 200
 
